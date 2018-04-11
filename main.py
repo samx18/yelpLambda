bd92bf5718d2287c5e781daa1fe2e5e918725d73
Sam <sampat.palani@gmail.com>
2018-04-11 16:43:42 -0700

    Parsing rating from response

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ API_KEY=config.API_KEY
 # make a request to get the data
 
 def getRequest(api_key):
-	url='https://api.yelp.com/v3/businesses/search?term=musical&location=ca&limit=1'
+	url='https://api.yelp.com/v3/businesses/search?term=musical&location=ca&limit=10'
 	headers = {
        'Authorization': 'Bearer %s' % api_key,
        }
@@ -21,14 +21,18 @@ def getRequest(api_key):
 # Parse data to dict
 
 def responseParse(jsonResponse):
+	respDict = json.loads(jsonResponse)
+
 
 # Traverse dict to get values
 
 # Store as a file in S3
-	return
+	return respDict
 
 def main():
-	print getRequest(API_KEY)
+	response=getRequest(API_KEY)
+	for i in  response['businesses']:
+		print i['rating']
 	return
 
 if __name__ == '__main__':
