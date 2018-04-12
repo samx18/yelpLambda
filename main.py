6dda491a366607336e684ea8f75a38f997d2daf4
Sam <sampat.palani@gmail.com>
2018-04-12 11:21:12 -0700

    Save output as csv

--- main.py
+++ main.py
@@ -11,28 +11,34 @@ API_KEY=config.API_KEY
 # make a request to get the data
 
 def getRequest(api_key):
-	url='https://api.yelp.com/v3/businesses/search?term=musical&location=ca&limit=10'
+	url='https://api.yelp.com/v3/businesses/search?term=musical&location=ca&limit=50'
 	headers = {
        'Authorization': 'Bearer %s' % api_key,
        }
  	response = requests.request('GET', url, headers=headers)
  	return response.json()
 
-# Parse data to dict
-
-def responseParse(jsonResponse):
-	respDict = json.loads(jsonResponse)
-
-
-# Traverse dict to get values
-
-# Store as a file in S3
-	return respDict
 
 def main():
+	
+	# Get respponse as a dict by passing API KEY 
+
 	response=getRequest(API_KEY)
+	
+	# Open a file for writing 
+	
+	file = open("rating.csv", "w")
+	
+	# Iterate through the dict to retrive specific values
+
 	for i in  response['businesses']:
-		print i['rating']
+		a = str(i['name'])+","+str(i['location']['city'])+","+str(i['location']['state'])+","+str(i['rating'])+"\n"
+		file.write(a)
+	
+	# Close file when done
+	
+	file.close()
+	
 	return
 
 if __name__ == '__main__':
